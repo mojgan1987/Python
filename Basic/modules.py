26dcaa689e24127c3fb8c1467c3116b25976d276
Mojgan Hashemian <mojgan1987@gmail.com>
2021-02-01 09:57:45 +0000

    Create modules.py
    collections

---
+++ Basic/modules.py
@@ -0,0 +1,20 @@
+## Some quick notes ##
+
+##
+# COllections
+
+# Counters: n of unique values in a list
+from colections import Counter
+tocheck = [some repated intems], or string
+Counter(tocheck) # out: a dictionary subclass
+
+c = Counter(bla)
+c.most_common(tops)
+
+# Defult Dictionary
+from colections import defaultdict
+d  = defaultdict(lambda: 0)
+d['bla'] # returns 0
+
+
+
