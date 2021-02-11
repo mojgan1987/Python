517b5bbf77631be38b4d80f07421bfb78cc8d38f
Mojgan Hashemian <mojgan1987@gmail.com>
2021-02-11 21:41:11 +0000

    Create Lessons.py
    1 BinaryGap

---
+++ Coding Challenge Websites/Codility/Lessons.py
@@ -0,0 +1,27 @@
+def solution(N):
+    # to-do:
+    # check if the number is power of 2
+        # return 0
+
+    # trivial solution
+    n = bin(N)
+
+    maxl = 0
+    i = -1
+    flag = True
+    length = 0
+    
+    for char in n[2:]:
+        i+=1
+        char = int(char)
+        if char == 0 and flag:
+            pass
+        elif char == 1 and flag:
+            flag = False    
+        elif char == 0 and not flag:
+            length +=1
+            # print(length)
+        elif char == 1 and not flag:
+            maxl = max(maxl,length)
+            length = 0
+    return maxl
