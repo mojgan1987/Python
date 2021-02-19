c83b5cbe9cc04676f252188dbe59597fc084ec27
Mojgan Hashemian <mojgan1987@gmail.com>
2021-02-19 19:07:22 +0000

    CountDiv & GenomicRangeQuery

--- Coding Challenge Websites/Codility/Lesson5_prefix_sums.py
+++ Coding Challenge Websites/Codility/Lesson5_prefix_sums.py
@@ -37,4 +37,57 @@ def mushroom_picker(A,k,m):
         mushrooms = max(mushrooms, mid_sum(pref_sum,left_p,right_p))
         
     return mushrooms
+
+
+##
+## CountDiv
+# Compute number of integers divisible by k in range [a..b].
+# 100% https://app.codility.com/demo/results/training4X6SN8-M7Z/
+def solution(A, B, K):
+    # write your code in Python 3.6
+    min_div = 0
+    if A%K==0:
+        min_div = A
+    else:
+        min_div = (A//K+1)*K
+    # print(min_div)
+    max_div = (B//K)*K
+    # print(max_div)
+
+    # print(int(((max_div-min_div)/K)+1))
+    return int((max_div-min_div)/K+1)
     
+    
+##
+# GenomicRangeQuery
+# Find the minimal nucleotide from a range of sequence DNA.
+# 100% https://app.codility.com/demo/results/trainingEW3B7M-MZY/
+def solution(S, P, Q):
+# write your code in Python 3.6
+# non-empty string
+# check size of P Q
+# if len(P) != len(Q):
+#     return -1
+
+M = len(P)
+results = [0]*M
+
+gens = {'A':1, 'C':2, 'G':3, 'T':4}
+
+if M == 1:
+    results[0] = gens[S[0]]
+    # print(results)
+
+for item in range(M):
+    # print(item)
+    st = S[P[item]:Q[item]+1]
+    if 'A' in st:
+        results[item] = 1
+    elif 'C' in st:
+        results[item] = 2
+    elif 'G' in st:
+        results[item] = 3
+    elif 'T' in st:
+        results[item] = 4
+return results
+
