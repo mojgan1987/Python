c42634947d53cef5b0f3bd557ff952238d02915f
Mojgan Hashemian <mojgan1987@gmail.com>
2021-02-28 15:29:05 +0000

    Create LinkedList.py
    delete and reverse

---
+++ Coding Challenge Websites/HackerRank/LinkedList.py
@@ -0,0 +1,50 @@
+# Delete a node
+def deleteNode(head, position):
+    
+    if head.next == None:
+        return None
+    
+    if position == 0:
+        return head.next
+    
+    node = head
+    for i in range(position-1):
+        node = node.next
+        
+    node.next = node.next.next
+    return head
+
+
+# Reverse
+def reverse(head):
+    if head == None:
+        return head        
+
+    prev = None
+    current = head
+    
+    while current != None:
+        # print(current.data)
+        # print(f'prev={prev.data}')
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+        
+    return prev
+
+  
+# Recursive reverse
+def reverse(head):
+#     print(head.data)
+    if head == None:
+        return head
+    if head.next == None:
+        return head
+    
+    curr_head = reverse(head.next)
+    
+    head.next.next = head
+    head.next = None
+
+    return curr_head
