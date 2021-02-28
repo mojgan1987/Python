ad235dba49c77a10c234f08ad8ad473d55987add
Mojgan Hashemian <mojgan1987@gmail.com>
2021-02-28 15:33:13 +0000

    insert

--- Coding Challenge Websites/HackerRank/LinkedList.py
+++ Coding Challenge Websites/HackerRank/LinkedList.py
@@ -48,3 +48,19 @@ def reverse(head):
     head.next = None
 
     return curr_head
+
+# Insert
+def insertNodeAtPosition(head, data, position):
+    node = head
+    if position == 0:
+        newNode = SinglyLinkedListNode(data)
+        newNode.next = head
+        return newNode
+    count = 1
+    while count < position and node:
+        count += 1
+        node = node.next
+    newNode = SinglyLinkedListNode(data)
+    newNode.next = node.next
+    node.next = newNode
+    return head
